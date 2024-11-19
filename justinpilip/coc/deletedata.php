53280934ba16f01e72efe55885bb172863284812
PhilipJustin <pgomarga@gmail.com>
2024-11-19 08:09:47 +0700

    Delete justinpilip directory

--- justinpilip/coc/deletedata.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'conn.php';
-
-if(isset($_GET['id'])){
-    $id = $_GET['id'];
-    mysqli_query($conn, "DELETE FROM ordercoc WHERE id='$id'");
-    header("Location: index.php");
-}
-?>
